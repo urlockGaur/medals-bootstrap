8961bbc8081bcfa833eb4070f1ff58efb0a1db81
jgrissom <jgrissom91@gmail.com>
2024-08-29 13:24:39 -0500

    [3.04] Silver, Bronze medals

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 class App extends Component {
     state = {
         countries: [
-            { id: 1, name: 'United States', gold: 2 },
-            { id: 2, name: 'China', gold: 3 },
-            { id: 3, name: 'Germany', gold: 0 },
+            { id: 1, name: 'United States', gold: 2, silver: 2, bronze: 3 },
+            { id: 2, name: 'China', gold: 3, silver: 1, bronze: 0 },
+            { id: 3, name: 'Germany', gold: 0, silver: 2, bronze: 2 },
         ],
         medals: [
             { id: 1, name: 'gold' },
+            { id: 2, name: 'silver' },
+            { id: 3, name: 'bronze' },
         ]
     }
     handleIncrement = (countryId, medalName) => {
