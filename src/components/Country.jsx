b63997c3d5e86c7da982e5e0d946a2ce35e874cb
jgrissom <jgrissom91@gmail.com>
2024-08-29 13:28:18 -0500

    [3.06] Country component - total medals each country

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import Medal from './Medal';
 
 class Country extends Component {
+    getMedalsTotal(country, medals) {
+        let sum = 0;
+        medals.forEach(medal => { sum += country[medal.name]; });
+        return sum;
+    }
     render() {
         const { country, medals, onIncrement, onDecrement } = this.props;
         return (
             <div className="country">
                 <div className="name">
                     {country.name}
+                    <span className="badge">
+                        {this.getMedalsTotal(country, medals)}
+                    </span>
                 </div>
                 {medals.map(medal =>
                     <Medal
