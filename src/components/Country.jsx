f6b3ec45a9b40621cc9a5ae26df67e3e587401d0
jgrissom <jgrissom91@gmail.com>
2024-08-29 12:52:56 -0500

    [2.03] Create button to decrement the medal count - Use conditional rendering to prevent medal count < 0

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -6,6 +6,7 @@ class Country extends Component {
         gold: this.props.country.gold,
     }
     handleIncrement = () => this.setState({ gold: this.state.gold + 1 });
+    handleDecrement = () => this.setState({ gold: this.state.gold - 1 });
     render() {
         return (
             <div className="country">
@@ -15,6 +16,7 @@ class Country extends Component {
                 <div className="medals">
                     gold medals: {this.state.gold}
                     <button onClick={this.handleIncrement}>+</button>
+                    <button disabled={this.state.gold === 0} onClick={this.handleDecrement}>-</button>
                 </div>
                 <hr />
             </div>
